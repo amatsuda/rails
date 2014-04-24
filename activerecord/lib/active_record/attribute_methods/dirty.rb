7c8c5481c733cfa053d672ea80552b8a5c6186be
Akira Matsuda <ronnie@dio.jp>
2014-04-25 03:20:57 +0900

    Partial insert should not skip "not null" columns that have null values
    When "not null" column was omitted from insert statement in MySQL non-strict mode, MySQL silently puts something instead of raising an error,
    for example, 0 for an integer column, '' for a varchar column, '0000-00-00' for a date column.
    This fix brings AR 3.2 behaviour back, so as to raise null constraint error when nil was given to a "not null" column.

--- activerecord/lib/active_record/attribute_methods/dirty.rb
+++ activerecord/lib/active_record/attribute_methods/dirty.rb
@@ -81,7 +81,11 @@ module ActiveRecord
       # Serialized attributes should always be written in case they've been
       # changed in place.
       def keys_for_partial_write
-        changed | (attributes.keys & self.class.serialized_attributes.keys)
+        changed | (attributes.keys & self.class.serialized_attributes.keys) | keys_for_not_null_columns_having_null_value
+      end
+
+      def keys_for_not_null_columns_having_null_value
+        self.class.columns.reject(&:null).map(&:name).select {|c| self[c].nil?}
       end
 
       def _field_changed?(attr, old, value)
