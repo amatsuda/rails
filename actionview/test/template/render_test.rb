039980478074c42a1cff88df0c16e75a4fb8825c
Akira Matsuda <ronnie@dio.jp>
2015-02-06 00:18:05 +0900

    Partial template name does no more have to be a valid Ruby identifier
    because the partial renderer would not create an lvar per each template since c67005f221f102fe2caca231027d9b11cf630484

--- actionview/test/template/render_test.rb
+++ actionview/test/template/render_test.rb
@@ -171,18 +171,12 @@ module RenderTestCases
     assert_equal "only partial", @view.render("test/partial_only", :counter_counter => 5)
   end
 
-  def test_render_partial_with_invalid_name
-    e = assert_raises(ArgumentError) { @view.render(:partial => "test/200") }
-    assert_equal "The partial name (test/200) is not a valid Ruby identifier; " +
-      "make sure your partial name starts with underscore, " +
-      "and is followed by any combination of letters, numbers and underscores.", e.message
+  def test_render_partial_with_number
+    assert_nothing_raised { @view.render(:partial => "test/200") }
   end
 
   def test_render_partial_with_missing_filename
-    e = assert_raises(ArgumentError) { @view.render(:partial => "test/") }
-    assert_equal "The partial name (test/) is not a valid Ruby identifier; " +
-      "make sure your partial name starts with underscore, " +
-      "and is followed by any combination of letters, numbers and underscores.", e.message
+    assert_raises(ActionView::MissingTemplate) { @view.render(:partial => "test/") }
   end
 
   def test_render_partial_with_incompatible_object
@@ -191,10 +185,11 @@ module RenderTestCases
   end
 
   def test_render_partial_with_hyphen
-    e = assert_raises(ArgumentError) { @view.render(:partial => "test/a-in") }
-    assert_equal "The partial name (test/a-in) is not a valid Ruby identifier; " +
-      "make sure your partial name starts with underscore, " +
-      "and is followed by any combination of letters, numbers and underscores.", e.message
+    assert_nothing_raised { @view.render(:partial => "test/a-in") }
+  end
+
+  def test_render_partial_with_emoji
+    assert_nothing_raised { @view.render(:partial => 'test/🍣') }
   end
 
   def test_render_partial_with_invalid_option_as
